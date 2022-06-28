137f94f48cb9c8c458deca4138ab4a69425a22d9
Jarrod Sanders <jarrod.sanders@ufl.edu>
2022-06-28 01:21:15 -0400

    added a timed demo that turns left and right

--- controller.py
+++ controller.py
@@ -1,14 +1,30 @@
 import visionShim as vs
 import robot
+import time
 
 if __name__ == "__main__":
-    vs.setup()
-    r = robot.Robot()
+    #vs.setup()
 
-    while(True):
+    r = robot.Robot(3,4,17,27)
+
+    startTime = time.monotonic()
+    runTime = time.monotonic() - startTime
+    while(runTime < 10):
         ballLR, isBall = vs.runVision()
-        print(f"{ballLR}, {isBall}")
 
-        inp = input("Enter to continue, q to quit: ")
-        if inp == "q":
-            exit()
+        message = ""
+        if(ballLR < 0.4):
+            r.turn(r.left)
+            message = "turning left"
+        elif(ballLR > 0.6):
+            r.turn(r.right)
+            message = "turning right"
+        else:
+            r.stop()
+            message = "Stopped"
+        runTime = time.monotonic() - startTime
+        print("BallLR: {:.2f} | {} | {:.2f}".format(ballLR, message, runTime))
+        time.sleep(0.1)
+
+    r.stop()
+    exit()
